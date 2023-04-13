7a1c13c93080231545f73157c12a23666796ef4a
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-04-13 15:28:04 +0300

    feat: решить задачу what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Extract season from given date and expose the enemy scout!
@@ -11,11 +11,29 @@ const { NotImplementedError } = require('../lib');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(/* date */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+
+  try {
+    date.getTime();
+  } catch (err) {
+    throw new Error("Invalid date!");
+  }
+
+  const month = date.getMonth();
+
+  const seasons = {
+    winter: [11, "Dec", 0, "Jan", 1, "Feb"],
+    spring: [2, "Mar", 3, "Apr", 4, "May"],
+    summer: [5, "June", 6, "July", 7, "Aug"],
+    autumn: [8, "Sept", 9, "Oct", 10, "Nov"],
+  };
+
+  for (let season in seasons) {
+    if (seasons[season].includes(month)) return season;
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
