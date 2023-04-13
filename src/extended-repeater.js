312af237f01d7b51309a736dab068f526b33ad9a
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-04-13 09:11:34 +0300

    feat: добавить функцию extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,29 @@ const { NotImplementedError } = require('../lib');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
+function repeater(str, options) {
+  let res = '';
+  const {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = options;
+  
+  for (let i = 0; i < repeatTimes; i++) {
+    res += String(str);
+    
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      res += String(addition);
+      
+      if (j < additionRepeatTimes - 1) res += additionSeparator;
+    }
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+    if (i < repeatTimes - 1) res += separator;
+  }
+  
+  return res;
 }
 
 module.exports = {
