a2dc7c1aa3307fe69d483511783f8f22be584936
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-04-14 16:29:35 +0300

    feat: решить задачу vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,90 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(boolean) {
+    this.boolean = boolean;
   }
-
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  
+  encrypt(message, key) {
+    console.debug(message, key)
+    if (!(message && key)) throw Error('Incorrect arguments!');
+    
+    const spaceIdxsMessage = [];
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === ' ') spaceIdxsMessage.push(i);
+    }
+    
+    let encryptKey = '';
+    const messageNoSpace = message.replace(/\s/g, '');
+    while (encryptKey.length < messageNoSpace.length) {
+      encryptKey += key;
+    }
+    encryptKey = encryptKey.slice(0, messageNoSpace.length);
+    
+    let encryptWord = '';
+    for (let i = 0; i < messageNoSpace.length; i++) {
+      let messageChar = messageNoSpace[i].toUpperCase().charCodeAt();
+      let encryptWordChar = encryptKey[i].toUpperCase().charCodeAt();
+      
+      if (messageChar < 65 || messageChar > 90) {
+        encryptWord += messageNoSpace[i];
+      } else {
+        let letter = messageChar + encryptWordChar - 65;
+        
+        if (letter > 90) letter -= 26;
+        encryptWord += String.fromCharCode(letter)
+      }
+    }
+    
+    const spacesBack = encryptWord.split('');
+    for (let i = 0; i < spacesBack.length; i++) {
+      if (spaceIdxsMessage.includes(i)) {
+        spacesBack.splice(i, 0, ' ');
+      }
+    }
+    
+    return this.boolean !== false ? spacesBack.join('') : spacesBack.reverse().join('');
+  }
+  
+  decrypt(encryptedMessage, key) {
+    console.log(encryptedMessage, key)
+    if (!(encryptedMessage && key)) throw Error('Incorrect arguments!');
+    
+    const spaceIdxsEncryptedMessage = [];
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (encryptedMessage[i] === ' ') spaceIdxsEncryptedMessage.push(i);
+    }
+    
+    let encryptKey = '';
+    const encryptedMessageNoSpace = encryptedMessage.replace(/\s/g, '');
+    while (encryptKey.length < encryptedMessageNoSpace.length) {
+      encryptKey += key;
+    }
+    encryptKey = encryptKey.slice(0, encryptedMessageNoSpace.length);
+    
+    let message = '';
+    for (let i = 0; i < encryptedMessageNoSpace.length; i++) {
+      let encryptedMessageChar = encryptedMessageNoSpace[i].toUpperCase().charCodeAt();
+      let encryptWordChar = encryptKey[i].toUpperCase().charCodeAt();
+      
+      if (encryptedMessageChar < 65 || encryptedMessageChar > 90) {
+        message += encryptedMessageNoSpace[i];
+      } else {
+        let letter = encryptedMessageChar - encryptWordChar + 65;
+        
+        if (letter < 65) letter += 26;
+        message += String.fromCharCode(letter)
+      }
+    }
+    
+    const spacesBack = message.split('');
+    for (let i = 0; i < spacesBack.length; i++) {
+      if (spaceIdxsEncryptedMessage.includes(i)) {
+        spacesBack.splice(i, 0, ' ');
+      }
+    }
+    
+    return this.boolean !== false ? spacesBack.join('') : spacesBack.reverse().join('');
   }
 }
 
